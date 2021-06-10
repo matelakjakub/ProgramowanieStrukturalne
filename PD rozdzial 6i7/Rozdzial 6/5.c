2f2a2601fc027d6125c10e58cd30cfaabf61e41a
matelakjakub <73526507+matelakjakub@users.noreply.github.com>
2021-06-10 23:11:03 +0200

    Add files via upload

---
+++ PD rozdzial 6i7/Rozdzial 6/5.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int a, b;
+    printf("Podaj dwie liczby calkowite i dwie granice: ");
+    scanf("%d %d", &a, &b);
+    for(int i = a; i <= b; i++){
+        printf("%d | %d | %d\n", i, i * i, i * i * i);
+    }
+    return 0;
+}
